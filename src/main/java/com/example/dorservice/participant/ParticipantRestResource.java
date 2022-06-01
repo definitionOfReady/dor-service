2fdbdf873b92b16831843fb796110d375aa5dda7
Marius Stalder <marius-stalder@msn.com>
2022-06-01 16:16:37 +0200

    update ParticipantRestResource

--- src/main/java/com/example/dorservice/participant/ParticipantRestResource.java
+++ src/main/java/com/example/dorservice/participant/ParticipantRestResource.java
@@ -1,13 +1,21 @@
 package com.example.dorservice.participant;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("participants")
 public class ParticipantRestResource {
+
+
+    @Autowired
+    HttpServletRequest httpServletRequest;
 
     private final ParticipantRepository participantRepository;
 
@@ -24,6 +32,20 @@ public class ParticipantRestResource {
     @GetMapping("/{id}")
     public Participant getParticipant(@PathVariable UUID id) {
         return new Participant();
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Participant> getParticipant() {
+        String header = httpServletRequest.getHeader("x-user-id");
+        if(header==null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        UUID userId = UUID.fromString(header);
+        Participant participant = participantRepository.findById(userId).orElse(null);
+        if(participant==null) {
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity(participant, HttpStatus.OK);
     }
 
 }
