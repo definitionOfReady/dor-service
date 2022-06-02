4ab2dd67474f1894bcbf05ce4f9d0f46236128fd
Marcus May (U119240) <marcus.may@mobi.ch>
2022-06-02 15:46:34 +0200

    change rest endpoints

--- src/main/java/com/example/dorservice/trip/TripRestResource.java
+++ src/main/java/com/example/dorservice/trip/TripRestResource.java
@@ -1,5 +1,7 @@
 package com.example.dorservice.trip;
 
+import com.example.dorservice.groupactivity.GroupActivityService;
+import com.example.dorservice.groupactivity.PredictedActivityDto;
 import com.example.dorservice.participant.Participant;
 import com.example.dorservice.participant.ParticipantRepository;
 import org.slf4j.Logger;
@@ -8,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.ws.rs.Produces;
+import java.util.List;
 import java.util.UUID;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("trip")
@@ -23,9 +28,11 @@ public class TripRestResource {
     @Autowired
     ParticipantRepository participantRepository;
 
+    @Autowired
+    GroupActivityService groupActivityService;
+
     @PostMapping
     public Trip create(@RequestBody CreateTripRequestDTO req) {
-//        LOGGER.info("create req={}", req);
         Participant participant = participantRepository.save(req.getInitialParticipant());
         req.getTrip().getParticipants().add(participant);
         return tripRepository.save(req.getTrip());
@@ -34,5 +41,24 @@ public class TripRestResource {
     @GetMapping("/{id}")
     public Trip get(@PathVariable UUID id) {
         return tripRepository.findById(id).get();
+    }
+
+
+    @PutMapping("/{id}/predict")
+    public List<PredictedActivityDto> createPredictActivity(@PathVariable UUID id) {
+        List<PredictedActivityDto> predictedActivityDtos = groupActivityService.predictGroupActivity(id);
+        Trip trip = tripRepository.findById(id).get();
+        trip.setGroupActivities(predictedActivityDtos.stream()
+                .map(PredictedActivityDto::getGroupActivity)
+                .collect(toList()));
+        tripRepository.save(trip);
+        return predictedActivityDtos;
+    }
+
+    @PutMapping("/{tid}/select/{aid}")
+    public void createPredictActivity(@PathVariable UUID tid, @PathVariable UUID aid) {
+        Trip trip = tripRepository.findById(tid).get();
+        trip.setSelectedActivityID(aid);
+        tripRepository.save(trip);
     }
 }
